1e2c304080b36b1f63337970383f48e567ea55ab
zhangchao53 <zhangchao53@xiaomi.com>
2023-04-06 17:39:33 +0800

    porting rlottie tests cases,add case references to main source file .
    Change-Id: Ibe192c30ac998de3e9a189ffa959d0a33f002021

--- test/testsuite.cpp
+++ test/testsuite.cpp
@@ -1,6 +1,11 @@
 #include <gtest/gtest.h>
 
-int main(int argc, char **argv) {
+#include "test_lottieanimation_capi.cpp"
+#include "test_lottieanimation.cpp"
+#include "test_vpath.cpp"
+#include "test_vrect.cpp"
+
+extern "C" int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
